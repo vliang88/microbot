c6ebb9de03c32cc75c0f29d9eac1dac8950e3a95
vliang88 <66654821+vliang88@users.noreply.github.com>
2024-06-04 22:42:19 -0400

    Added host 2 support
    Fix Overlay to display more details

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDBurnerLighter/DDBurnerLighterOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDBurnerLighter/DDBurnerLighterOverlay.java
@@ -8,6 +8,15 @@ import net.runelite.client.ui.overlay.components.TitleComponent;
 
 import javax.inject.Inject;
 import java.awt.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import static net.runelite.client.plugins.natepainthelper.Info.timeBegan;
+import static net.runelite.client.plugins.natepainthelper.Info.xpGained;
+
 
 public class DDBurnerLighterOverlay extends OverlayPanel {
     @Inject
@@ -17,21 +26,43 @@ public class DDBurnerLighterOverlay extends OverlayPanel {
         setPosition(OverlayPosition.TOP_LEFT);
         setNaughty();
     }
+    public long timeStart = System.currentTimeMillis();
     @Override
     public Dimension render(Graphics2D graphics) {
         try {
-            panelComponent.setPreferredSize(new Dimension(200, 300));
+            long timeRanMs = System.currentTimeMillis() -timeStart;
+            long timeRanHr = timeRanMs/3600000;
+            long timeRanMin = (timeRanMs % 3600000)/60000;
+            long timeRanSec = (timeRanMs % 60000) / 1000;
+
+            panelComponent.setPreferredSize(new Dimension(300, 500));
             panelComponent.getChildren().add(TitleComponent.builder()
-                    .text("Micro Example V" + DDBurnerLighterScript.version)
+                    .text("DDBurnerLighterScript" + DDBurnerLighterScript.version)
                     .color(Color.GREEN)
                     .build());
 
-            panelComponent.getChildren().add(LineComponent.builder().build());
-
             panelComponent.getChildren().add(LineComponent.builder()
-                    .left(Microbot.status)
                     .build());
 
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Time Ran: " + timeRanHr + ":" + timeRanMin + ":" + timeRanSec )
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Current State: " + DDBurnerLighterScript.currentState)
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Time Left for " + DDBurnerLighterScript.host1 +
+                            (Microbot.getClient().getTickCount() - DDBurnerLighterScript.lightStartTimestamp[0]
+                                    - DDBurnerLighterScript.incenseBurnDurationTick) + "ticks")
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Time Left for " + DDBurnerLighterScript.host2 +
+                            (Microbot.getClient().getTickCount() - DDBurnerLighterScript.lightStartTimestamp[1]
+                                    - DDBurnerLighterScript.incenseBurnDurationTick) + "ticks")
+                    .build());
 
         } catch(Exception ex) {
             System.out.println(ex.getMessage());
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDBurnerLighter/DDBurnerLighterScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDBurnerLighter/DDBurnerLighterScript.java
@@ -2,7 +2,6 @@ package net.runelite.client.plugins.microbot.DDBurnerLighter;
 
 import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.widgets.Widget;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.util.camera.Rs2Camera;
@@ -14,9 +13,10 @@ import net.runelite.client.plugins.microbot.util.player.Rs2Player;
 import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 
 enum states{
@@ -27,10 +27,15 @@ enum states{
     state_atPhails
 }
 public class DDBurnerLighterScript extends Script {
+    //For overlay
     public static double version = 1.0;
-    public long incenseBurnDurationTick;
-    public long scriptStartTimestamp;
-    public long lightStateTimestamp;
+    public static String host1;
+    public static String host2;
+
+    public static int incenseBurnDurationTick;
+    public int randNum;
+    public static long[] lightStartTimestamp = new long[]{0,0};
+    public int hostNumber = 0;
     //ITEM IDS
     public static int notedMarentillId = 252;
     public static int marentillId = 251;
@@ -40,28 +45,31 @@ public class DDBurnerLighterScript extends Script {
     public static int POHExitId = 4525;
     public static int burnerId = 13213;
     public static int unlitBurnerId = 13212;
-    public static int alterId = 40879;
+    public static int alterId = 13197;
 
     //WORLD POINTS
-    WorldPoint POHWorldPoint = new WorldPoint(2955 , 3224, 0);
-    WorldPoint PhialsWorldPoint = new WorldPoint(2950 , 3214, 0);
+    WorldPoint POHWorldPoint = new WorldPoint(2953, 3223, 0);
+    WorldPoint PhialsWorldPoint = new WorldPoint(2951, 3216, 0);
     public int POHWorldPointSize = 5;
     public static boolean fullyInit = false;
     //Script states
-    states currentState = states.state_init;
+    public static states currentState = states.state_init;
+
     public boolean run(DDBurnerLighterConfig config) {
         Microbot.enableAutoRunOn = false;
-        incenseBurnDurationTick = 200 + Microbot.getClient().getRealSkillLevel(Skill.FIREMAKING);
+        host1 = config.message1();
+        host2 = config.message2();
+        incenseBurnDurationTick = 150 + Microbot.getClient().getRealSkillLevel(Skill.FIREMAKING) - randNum;
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
                 if (!Microbot.isLoggedIn()) return;
                 if (!super.run()) return;
                 //TODO:Check to make sure that the account has at least 35FM or else return
                 //Make sure we not going nuts when the player is walking
-                while(Rs2Player.isWalking()){
+                while (Rs2Player.isWalking()) {
                     sleep(100);
                 }
-                switch(currentState) {
+                switch (currentState) {
                     case state_init:
                         //make sure we are on world 330
                         if (Microbot.getClient().getWorld() != 330) {
@@ -71,73 +79,98 @@ public class DDBurnerLighterScript extends Script {
                         //Change the minimap zoom and make top down camera
                         Microbot.getClient().setMinimapZoom(2.0);
                         Microbot.getClient().setScalingFactor(272);
-                        Rs2Camera.setPitch(100);
+                        if(Microbot.getClient().getScale() > 300) {
+                            Microbot.getMouse().scrollDown(new Point(Rs2Player.getLocalLocation().getX(), Rs2Player.getLocalLocation().getY()));
+                        }
+                            Rs2Camera.setPitch(100);
                         //Open up inventory
-                        if(!Rs2Inventory.isOpen())
+                        if (!Rs2Inventory.isOpen())
                             Rs2Inventory.open();
 
                         //check to make sure we have all item and choose next state
                         if (!Rs2Inventory.contains(notedMarentillId) || !Rs2Inventory.contains("Coins") || !Rs2Inventory.contains(tinderboxId)) {
-                            System.out.println("Inventory Missing Item");
-                        }else{
+                            Microbot.showMessage("Inventory Missing Item");
+                        } else {
                             //All guest houses are plane 1
-                            if(!isInHouse()){
+                            if (!isInHouse()) {
                                 currentState = states.state_walkToHousePortal;
-                            }else{
+                            } else {
                                 currentState = states.state_atHouse;
                             }
                         }
                         break;
                     case state_walkToHousePortal:
+                        if (isInHouse()) {
+                            currentState = states.state_atHouse;
+                            break;
+                        }
+
                         //Walk to the portal if we are not currently at the portal
-                        if(Rs2Player.getWorldLocation().distanceTo2D(POHWorldPoint) > POHWorldPointSize) {
+                        if (Rs2Player.getWorldLocation().distanceTo2D(POHWorldPoint) > POHWorldPointSize) {
                             //Not inside the area for advertising, Walk to advertising place
                             if (!Rs2Player.isWalking()) {
                                 Rs2Walker.walkTo(POHWorldPoint, 0);
-                            }else{
-                                if(Microbot.getClient().getEnergy() > 20){
+                            } else {
+                                if (Microbot.getClient().getEnergy() > 20) {
                                     Rs2Player.toggleRunEnergy(true);
                                 }
                             }
-                        }else{//We are currently at the portal, go to state to see if we go into house
+                        } else {//We are currently at the portal, go to state to see if we go into house
                             currentState = states.state_atPortal;
                         }
                         break;
                     case state_atPortal:
                         //Make sure we are at the portal area in this state or we go back
-                        if(Rs2Player.getWorldLocation().distanceTo2D(POHWorldPoint) > POHWorldPointSize) {
+                        if (Rs2Player.getWorldLocation().distanceTo2D(POHWorldPoint) > POHWorldPointSize) {
                             currentState = states.state_walkToHousePortal;
                             break;
                         }
                         //Check if we have more than 2 maretill in our inventory
-                        if(Rs2Inventory.count(marentillId) >=2 ){
+                        if (Rs2Inventory.count(marentillId) >= 2) {
                             //We have enough, lets go into a portal
-                            enterPortal();
-                            currentState = states.state_atHouse;
-                        }else{
-                            //We dont have enough herbs, go unnote
-                            if(!Rs2Npc.hasLineOfSight(Rs2Npc.getNpc("Phials"))){
-                                Rs2Walker.walkTo(PhialsWorldPoint, 0);
+                            //Check which host we have to go to
+                            if(lightStartTimestamp[0] > lightStartTimestamp[1]){
+                                hostNumber = 1;
+                            }else{
+                                hostNumber = 0;
                             }
+                            enterPortal(config, hostNumber);
+                            currentState = states.state_atHouse;
+                        } else {
+                            //We dont have enough herbs, go unnote
+                            //if (!Rs2Npc.hasLineOfSight(Rs2Npc.getNpc("Phials"))) {
+                                Rs2Walker.walkTo(PhialsWorldPoint, 0);
+                            //}
                             currentState = states.state_atPhails;
                         }
                         break;
                     case state_atPhails:
                         unoteMarentill();
-                        if(!Rs2GameObject.exists(POHId)) {
-                            Rs2Walker.walkTo(POHWorldPoint);
+                        if (!Rs2GameObject.exists(POHId)) {
+                            Rs2Walker.walkFastCanvas(POHWorldPoint);
                         }
                         currentState = states.state_atPortal;
                         break;
 
                     case state_atHouse:
-                        if(!isInHouse()){
+                        if (!isInHouse()) {
                             currentState = states.state_walkToHousePortal;
                             break;
                         }
-                        if(Rs2Inventory.count(marentillId) >=2 ){
-                            sleep(1000);
-                        }else{
+                        if (Rs2Inventory.count(marentillId) >= 2) {
+                            if ((lightStartTimestamp[hostNumber] == 0)
+                                    || ((Microbot.getClient().getTickCount() - lightStartTimestamp[hostNumber]) >= (incenseBurnDurationTick))) {
+                                lightStartTimestamp[hostNumber] = Microbot.getClient().getTickCount();
+                                lightBurners();
+                                exitHousePortal();
+                                currentState = states.state_atPortal;
+                                break;
+                            }
+                            //Only run random antiban half the time
+                            if(Math.random() < 0.02){
+                                runAntiban();
+                            }
+                        } else {
                             exitHousePortal();
                             currentState = states.state_atPortal;
                         }
@@ -149,7 +182,7 @@ public class DDBurnerLighterScript extends Script {
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
-        }, 0, 1000, TimeUnit.MILLISECONDS);
+        }, 1000, 1000, TimeUnit.MILLISECONDS);
         return true;
     }
 
@@ -158,32 +191,77 @@ public class DDBurnerLighterScript extends Script {
         super.shutdown();
     }
 
-    private void unoteMarentill(){
+    private void unoteMarentill() {
         //Click on the noted maretill in inventory
         Rs2Inventory.interact(notedMarentillId, "Use");
-        sleep(250,1000);
+        sleep(250, 1000);
         Rs2Npc.interact(NpcID.PHIALS, "Use");
-        sleep(250,1000);
+        sleep(250, 1000);
         sleepUntil(() -> Rs2Widget.hasWidget("Exchange All"));
-        sleep(250,1000);
+        sleep(250, 1000);
         Rs2Widget.clickWidget("Exchange All");
     }
-    private void enterPortal(){
+
+    private void enterPortal(DDBurnerLighterConfig config, int hostNumber) {
         Rs2GameObject.interact(POHId, "Friend's House");
-        sleep(250,1000);
+        sleep(250, 1000);
         sleepUntil(() -> Rs2Widget.hasWidget("Enter Name"));
-        sleep(250,1000);
-        Rs2Keyboard.typeString("workless");
+        sleep(250, 1000);
+        if(hostNumber == 0) {
+            Rs2Keyboard.typeString(config.message1());
+        }else{
+            Rs2Keyboard.typeString(config.message2());
+        }
         Rs2Keyboard.enter();
-        sleepUntil(()->Rs2Player.getWorldLocation().getPlane() != 0);
+        sleepUntil(() -> Rs2Player.getWorldLocation().getPlane() != 0);
+        //Choose a new random number for this house
+        randNum = (int)(Math.random()*10) *2;
     }
-    private void exitHousePortal(){
+
+    private void exitHousePortal() {
         Rs2Camera.turnTo(Rs2GameObject.findObjectById(POHExitId));
         Rs2GameObject.interact(POHExitId, "Enter");
-        sleepUntil(()->Rs2Player.getWorldLocation().getPlane() == 0);
+        sleepUntil(() -> Rs2Player.getWorldLocation().getPlane() == 0);
     }
-    private boolean isInHouse(){
+
+    private boolean isInHouse() {
         return Rs2GameObject.exists(POHExitId) || Rs2GameObject.exists(alterId)
                 || Rs2GameObject.exists(burnerId) || Rs2GameObject.exists(unlitBurnerId);
+    }
+
+    private void lightBurners() {
+        List<GameObject> burners = Rs2GameObject.getGameObjects()
+                .stream()
+                .filter(x -> x.getId() == burnerId || x.getId() == unlitBurnerId)
+                .sorted(Comparator.comparingInt(x -> Microbot.getClient().getLocalPlayer().getWorldLocation().distanceTo(x.getWorldLocation())))
+                .collect(Collectors.toList());
+
+        if (burners.get(0) != null) {
+            Rs2GameObject.interact(burners.get(0), "Light");
+            sleep(2000);
+            sleepUntil(() -> !Rs2Player.isWalking() && !Rs2Player.isAnimating());
+        }
+
+        if (burners.get(1) != null) {
+            Rs2GameObject.interact(burners.get(1), "Light");
+            sleep(2000);
+            sleepUntil(() -> !Rs2Player.isWalking() && !Rs2Player.isAnimating());
+        }
+    }
+    private void runAntiban(){
+        int Min = 1;
+        int Max = 2;
+        int randNum = Min + (int)(Math.random() * ((Max - Min) + 1));
+        System.out.println("Antiban: " + randNum);
+        switch (randNum){
+            case 1:
+                Rs2GameObject.interact("Rejuvenation", "Drink");
+                break;
+            case 2:
+                Rs2GameObject.interact("Altar", "Pray");
+                break;
+            default:
+                break;
+        }
     }
 }
