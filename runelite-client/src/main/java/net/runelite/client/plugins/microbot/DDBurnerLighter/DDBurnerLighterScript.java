cc23788a5001a7be5e1e362b77d737203b37bb12
vliang88 <66654821+vliang88@users.noreply.github.com>
2024-06-06 22:28:48 -0400

    Update

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDBurnerLighter/DDBurnerLighterScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDBurnerLighter/DDBurnerLighterScript.java
@@ -115,8 +115,9 @@ public class DDBurnerLighterScript extends Script {
                         //check if we are using 2 host
                         if (isTimeToLightBurner()) {
                             lightBurners();
-                            if(config.useSecondHost()) //Dont exit if we are only using 1 because stupid to exit
+                            if(config.useSecondHost()) { //Dont exit if we are only using 1 because stupid to exit
                                 exitHousePortal();
+                            }
                         } else {
                             runAntiban();
                             comment = "Afking in " + hostName + "'s House";
