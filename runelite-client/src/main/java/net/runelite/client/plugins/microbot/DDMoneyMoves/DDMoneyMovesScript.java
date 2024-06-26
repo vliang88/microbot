2d1e86161da68d7f08b5ec153b889cb3bc030d3c
vliang88 <66654821+vliang88@users.noreply.github.com>
2024-06-26 16:06:18 -0400

    Blast furnace goes to varrock at end of the run. Money move put player in GE

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDMoneyMoves/DDMoneyMovesScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/DDMoneyMoves/DDMoneyMovesScript.java
@@ -3,6 +3,10 @@ package net.runelite.client.plugins.microbot.DDMoneyMoves;
 import net.runelite.client.plugins.microbot.DDBlastFurnace.DDBlastFurnaceScript;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.util.bank.enums.BankLocation;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
+
 import java.util.concurrent.TimeUnit;
 
 public class DDMoneyMovesScript extends Script {
@@ -12,7 +16,11 @@ public class DDMoneyMovesScript extends Script {
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             if (!super.run()) return;
             try {
-                //DDBlastFurnaceScript.masterOnSwitch= true;
+                if(!DDBlastFurnaceScript.masterOnSwitch) {
+                    if(!Rs2Walker.isInArea(BankLocation.GRAND_EXCHANGE.getWorldPoint(),5)){
+                        Rs2Walker.walkTo(BankLocation.GRAND_EXCHANGE.getWorldPoint());
+                    }
+                }
                 sleep(1000);
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
